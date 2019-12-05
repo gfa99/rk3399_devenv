f09cdf743e7147569a523a6cf8a3a8e9d2311514
Anders <anders.du@robotemi.com>
2019-12-05 17:45:56 +0800

    Updated burn script.
    Signed-off-by: Anders <anders.du@robotemi.com>

--- burn.sh
+++ burn.sh
@@ -1,15 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
+sudo ./tools/upgrade_tool ul output/rk3399_loader_v1.22.115.bin
+sudo ./tools/upgrade_tool di -p output/parameter.txt
+sudo ./tools/upgrade_tool di uboot output/uboot.img output/parameter.txt
 
-sudo ./upgrade_tool ul rk3399_loader_v1.22.115.bin
-sudo ./upgrade_tool di -p parameter.txt
-sudo ./upgrade_tool di uboot uboot.img parameter.txt
-sudo ./upgrade_tool di trust trust.img parameter.txt
-sudo ./upgrade_tool di -m misc_zero.img parameter.txt
-sudo ./upgrade_tool di resource resource.img parameter.txt
-sudo ./upgrade_tool di -k kernel.img parameter.txt
-sudo ./upgrade_tool di boot boot.img parameter.txt
- 
+sudo ./tools/upgrade_tool di -k output/kernel.img output/parameter.txt
+sudo ./tools/upgrade_tool di resource output/resource.img output/parameter.txt
+sudo ./tools/upgrade_tool rd
 
-#sudo ./upgrade_tool di userdata ubuntu-rootfs.img parameter.txt
-sudo ./upgrade_tool rd
