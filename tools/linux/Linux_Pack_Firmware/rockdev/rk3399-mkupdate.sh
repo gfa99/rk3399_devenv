abe0c666e64978655483916d4aa29d719b866bfa
Anders Du <anders.du@robotemi.com>
2020-07-03 17:17:18 +0800

    优化升级包的打包/解包脚本

--- tools/linux/Linux_Pack_Firmware/rockdev/rk3399-mkupdate.sh
+++ tools/linux/Linux_Pack_Firmware/rockdev/rk3399-mkupdate.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
+
+IMG_NAME="update.img"
+
 pause()
 {
-echo "Press any key to quit:"
-read -n1 -s key
-exit 1
+	echo "Press any key to quit:"
+	read -n1 -s key
+	exit 1
 }
-echo "start to make update.img..."
+
+if [ -n "$1" ]; then
+    IMG_NAME="$1"
+fi
+
+echo "start to make ${IMG_NAME} ..."
+
 if [ ! -f "Image/parameter" -a ! -f "Image/parameter.txt" ]; then
 	echo "Error:No found parameter!"
 	exit 1
 fi
+
 if [ ! -f "package-file" ]; then
 	echo "Error:No found package-file!"
 	exit 1
 fi
-./afptool -pack ./ Image/update.img || pause
-./rkImageMaker -RK330C Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
-echo "Making ./Image/update.img OK."
-#echo "Press any key to quit:"
-#read -n1 -s key
+
+./afptool -pack ./ Image/${IMG_NAME} || pause
+./rkImageMaker -RK330C Image/MiniLoaderAll.bin Image/${IMG_NAME} ${IMG_NAME} -os_type:androidos || pause
+
+# update.img is new format, Image/update.img is old format, so delete older format
+rm -f Image/${IMG_NAME}
+
+echo "Making ${IMG_NAME} OK."
+echo "Press any key to quit:"
+read -n1 -s key
 exit $?
--- tools/linux/Linux_Pack_Firmware/rockdev/unpack.sh
+++ tools/linux/Linux_Pack_Firmware/rockdev/unpack.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
+
+IMG_NAME="update.img"
+
 pause()
 {
-echo "Press any key to quit:"
-read -n1 -s key
-exit 1
+    echo "Press any key to quit:"
+    read -n1 -s key
+    exit 1
 }
-echo "start to unpack update.img..."
+
+if [ -n "$1" ]; then
+    IMG_NAME="$1"
+fi
+
+echo "start to unpack ${IMG_NAME} ..."
+
 if [ ! -d "output" ]; then
 	mkdir output
 fi
-if [ ! -f "update.img" ]; then
-	echo "Error:No found update.img!"
+
+if [ ! -f ${IMG_NAME} ]; then
+	echo "Error:No found ${IMG_NAME}!"
 	pause
 fi
-./rkImageMaker -unpack update.img output || pause
+
+./rkImageMaker -unpack ${IMG_NAME} output || pause
 ./afptool -unpack output/firmware.img output || pause
 rm -f output/firmware.img
 rm -f output/boot.bin
-echo "Unpacking update.img OK."
+
+echo "Unpacking ${IMG_NAME} OK."
 echo "Press any key to quit:"
 read -n1 -s key
 exit 0
