c5fef426aea2983fa5e788d931117a3082501392
Kevin Zhou <kevin.zhou@robotemi.com>
2021-07-12 15:50:07 +0800

    sync rockchip offical sdk [debian]:
    from (fa231213257e){2019-11-29 10:31:37} overlay-firmware: update npu_upgrade_pcie)
    to   (b87dbc4db19f){2021-04-30 11:32:26} overlay: enable glamor for xserver by default)

--- debian/overlay/usr/local/bin/drm-hotplug.sh
+++ debian/overlay/usr/local/bin/drm-hotplug.sh
@@ -1,47 +1,71 @@
-#!/bin/sh -x
-
-# Try to figure out XAUTHORITY and DISPLAY
-for pid in $(pgrep X 2>/dev/null || ls /proc|grep -ow "[0-9]*"|sort -rn); do
-    PROC_DIR=/proc/$pid
-
-    # Filter out non-X processes
-    readlink $PROC_DIR/exe|grep -qwE "X$|Xorg$" || continue
-
-    # Parse auth file and display from cmd args
-    export XAUTHORITY=$(cat $PROC_DIR/cmdline|tr '\0' '\n'| \
-        grep -w "\-auth" -A 1|tail -1)
-    export DISPLAY=$(cat $PROC_DIR/cmdline|tr '\0' '\n'| \
-        grep -w "^:.*" || echo ":0")
-
-    echo Found auth: $XAUTHORITY for dpy: $DISPLAY
-    break
-done
+#!/bin/bash -x
 
 export DISPLAY=${DISPLAY:-:0}
+export XUSER=${XUSER:-root}
 
-# Find an authorized user
-unset USER
-for user in root $(users);do
-    sudo -u $user xdpyinfo &>/dev/null && \
-        { USER=$user; break; }
-done
-[ $USER ] || exit 0
+function prepare_env() {
+    sudo -u $XUSER xdpyinfo &>/dev/null && return
 
-# Find connected monitors
-MONITORS=$(sudo -u $user xrandr|grep -w connected|cut -d' ' -f1)
+    # Try to figure out XAUTHORITY and DISPLAY
+    for pid in $(pgrep X 2>/dev/null || \
+        ls /proc|grep -ow "[0-9]*"|sort -rn); do
+        PROC_DIR=/proc/$pid
 
-# Make sure every connected monitors been enabled with a valid mode.
-for monitor in $MONITORS;do
-    # Unlike the drm driver, X11 modesetting drv uses HDMI for HDMI-A
-    CRTC=$(echo $monitor|sed "s/HDMI\(-[^B]\)/HDMI-A\1/")
+        # Filter out non-X processes
+        readlink $PROC_DIR/exe|grep -qwE "X$|Xorg$" || continue
+
+        # Parse auth file and display from cmd args
+        export XAUTHORITY=$(cat $PROC_DIR/cmdline|tr '\0' '\n'| \
+            grep -w "\-auth" -A 1|tail -1)
+        export DISPLAY=$(cat $PROC_DIR/cmdline|tr '\0' '\n'| \
+            grep -w "^:.*" || echo ":0")
+
+        echo Found auth: $XAUTHORITY for dpy: $DISPLAY
+        break
+    done
+
+    # Find an authorized user
+    for XUSER in root $(users);do
+        sudo -u $XUSER xdpyinfo &>/dev/null && return
+    done
+
+    exit 0
+}
+
+function xrandr_wrapper() {
+    sudo -u $XUSER xrandr --screen ${SCREEN:-0} $@
+}
+
+function handle_monitor() {
+    # X11 modesetting drv uses HDMI for HDMI-A
+    CRTC=$(echo $MONITOR|sed "s/HDMI\(-[^B]\)/HDMI-A\1/")
 
     SYS="/sys/class/drm/card*-$CRTC/"
 
-    # Already got a valid mode
-    grep -w "$(cat $SYS/mode)" $SYS/modes && continue
+    # Make sure every connected monitors been enabled with a valid mode.
+    if grep -wq connected $SYS/status; then
+        # Already got a valid mode
+        grep -wq "$(cat $SYS/mode)" $SYS/modes && return 0
 
-    # Ether disabled or wrongly configured
-    sudo -u $user xrandr --output $monitor --auto
+        # Ether disabled or wrongly configured
+        xrandr_wrapper --output $MONITOR --auto
+    else
+        # The DP needs reinit every time, so turn it off
+        [ $MONITOR == DP ] && \
+            xrandr_wrapper --output $MONITOR --off
+    fi
+}
+
+prepare_env
+
+SCREENS=$(sudo -u $XUSER xdpyinfo|grep screens|cut -d':' -f2)
+for SCREEN in $(seq 0 ${SCREENS:-0}); do
+    # Find monitors of screen
+    MONITORS=$(xrandr_wrapper 2>/dev/null|grep connect|cut -d' ' -f1)
+
+    for MONITOR in $MONITORS;do
+        handle_monitor
+    done
 done
 
 exit 0
